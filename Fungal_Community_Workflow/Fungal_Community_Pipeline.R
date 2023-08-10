30d82be35e8bdab5a951f5f39c548fd4e3a3ae32
Lin <erika.lin@ubc.ca>
2023-08-10 11:33:03 -0700

    Edit after function debug

--- Fungal_Community_Workflow/Fungal_Community_Pipeline.R
+++ Fungal_Community_Workflow/Fungal_Community_Pipeline.R
@@ -11,14 +11,14 @@ bacteria <- "./silva_nr99_v138.1_train_set.fa.gz" # file name for bacterial refe
 eukaryotes <- "./sh_general_release_dynamic_all_29.11.2022.fasta" # file name for all eukaryotes reference FASTA
 
 # inputs for ps.net2
-fastq.Fs <- "SuRDC_seq/S1_S354_L001_R1_001.fastq.gz"
-fastq.Rs <- "SuRDC_seq/S1_S354_L001_R2_001.fastq.gz"
+fastq.Fs <- "CC_seq/S1_S354_L001_R1_001.fastq.gz"
+fastq.Rs <- "CC_seq/S1_S354_L001_R2_001.fastq.gz"
 filtFs <- "filtered/S1_F_filt.fastq.gz"
 filtRs <- "filtered/S1_R_filt.fastq.gz"
 
 # inputs for ps.net
-fastq.fwd <- "SuRDC_seq/S1_S354_L001_R1_001.fastq.gz"
-fastq.rev <- "SuRDC_seq/S1_S354_L001_R2_001.fastq.gz"
+fastq.fwd <- "CC_seq/S1_S354_L001_R1_001.fastq.gz"
+fastq.rev <- "CC_seq/S1_S354_L001_R2_001.fastq.gz"
 filt.fwd <- "filtered/S1_F_filt.fastq.gz"
 filt.rev <- "filtered/S1_R_filt.fastq.gz"
 
@@ -31,16 +31,24 @@ S1_test <- ps.net(fastq.fwd, fastq.rev, filt.fwd, filt.rev,
 
 list(sample.ID = "S1", site = "SuRDC", block = 1, plot = 27, trt = 16, cover.crop = "Crescendo Ladino Clover")
 
+# TEST FILTERING
+
+fastq.Fs <- sort(list.files(seq_path, pattern="_R1_001.fastq.gz"))
+fastq.Rs <- sort(list.files(seq_path, pattern="_R2_001.fastq.gz"))
+fastq.Fs <- file.path(seq_path, fastq.Fs)
+fastq.Rs <- file.path(seq_path, fastq.Rs)
+
+
 
 # TEST: All Samples ----
 
 # Load sample metadata
-samdf <- as.list(read.csv("C:/Users/elinrg26/OneDrive - UBC/Research/GitHub/Cover Crop Study/Fungal Community Workflow/Mapping file for ITS sequencing.csv"))
+samdf <- read.csv("C:/Users/elinrg26/OneDrive - UBC/Research/GitHub/Cover Crop Study/Fungal_Community_Workflow/Mapping file for ITS sequencing.csv")
 # Load reference FASTA file for taxonomic classification
 fungi <- "./sh_general_release_dynamic_29.11.2022.fasta" # file name for fungal reference FASTA
 
 # Define filter path for the forward and reverse reads of each sample fastq file
-seq_path <- "./Rosa_Seq"  # Replace with the directory for fastq file after unzipping the folder
+seq_path <- "./CC_Seq"  # Replace with the directory for fastq file after unzipping the folder
 
 # Sort forward and reverse reads to be in the same order
 fnFs <- sort(list.files(seq_path, pattern="_R1_001.fastq.gz"))
@@ -49,11 +57,33 @@ fnRs <- sort(list.files(seq_path, pattern="_R2_001.fastq.gz"))
 fnFs <- file.path(seq_path, fnFs)
 fnRs <- file.path(seq_path, fnRs)
 
+# Extract sample names, assuming file paths have format: FOL_DER/SAMPLENAME_XXX.fastq.gz OR FOL_DER/SAMPLENAME_XXX.fastq
+sNames <- sapply(strsplit(fnFs, "_"), `[`, 2)
+sNames <- sapply(strsplit(sNames, "/"), `[`, 2)
+
 # Define file names for filtered fastq.gz files and assign file paths
 filt_path <- file.path(seq_path, "filtered") # Place filtered files in new subdirectory if one does not already exist
 if(!file_test("-d", filt_path)) dir.create(filt_path)
-filtFs <- file.path(filt_path, paste0(sampleNames, "_F_filt.fastq.gz"))
-filtRs <- file.path(filt_path, paste0(sampleNames, "_R_filt.fastq.gz"))
+filtFs <- file.path(filt_path, paste0(sNames, "_F_filt.fastq.gz"))
+filtRs <- file.path(filt_path, paste0(sNames, "_R_filt.fastq.gz"))
+
+
+filter.args = list(truncLen = c(240,160),
+                   maxN = 0,
+                   maxEE = c(2,2),
+                   truncQ = 2,
+                   rm.phix = TRUE, 
+                   compress = TRUE,
+                   multithread = FALSE)
+
+out <- dada2::filterAndTrim(fnFs, filtFs, fnRs, filtRs,
+                            truncLen = filter.args$truncLen,
+                            maxN = filter.args$maxN,
+                            maxEE = filter.args$maxEE,
+                            truncQ = filter.args$truncQ,
+                            rm.phix = filter.args$rm.phix,
+                            compress = filter.args$compress,
+                            multithread = filter.args$multithread)
 
 # Empty list for storing the results
 RESULTS <- list()
@@ -73,41 +103,68 @@ for (i in 1:length(fnFs)){
   # Create a new row of results for each output from the `ps.net`
   RESULTS[[i]] <- ps.net(fastq.Fs = fnFs[i],
                          fastq.Rs = fnRs[i], # file paths for forward and reverse raw fastq files
-                         filtFs,
-                         filtRs, # file paths for filtered fastq files
+                         filtFs = filtFs[i],
+                         filtRs = filtRs[i], # file paths for filtered fastq files
                          refFasta = fungi,
                          metadata = meta_data[[j]],
+                         # filter.args = list(truncLen = c(240,160),
+                         #                    maxN = 0,
+                         #                    maxEE = c(2,2),
+                         #                    truncQ = 2,
+                         #                    rm.phix = TRUE, 
+                         #                    compress = TRUE,
+                         #                    multithread = FALSE),
+                         tree.args = list(k = 4, 
+                                          inv = 0.2,
+                                          model = "GTR",
+                                          rearrangement = "stochastic"), 
                          network.args = list(type = "taxa",
                                              distance = "jaccard",
                                              max.dist = 0.55,
                                              keep.isolates = TRUE))
   
   # Save the results as they are produced, in case of crashes/errors
-  save(RESULTS, file = "RDS/preliminary_results_covercrop.rda")
+  save(RESULTS, file = "RDS/preliminary_results_CC_test.rda")
 }
 
+# Remove Greenhouse samples [[44:141]]
+RESULTS <- RESULTS[c(1:43, 142:221)]
+# Save modified `ps.net` output
+save(RESULTS, file = "RDS/results_covercrop_noGreenhouse.rda")
 
-test <- readRDS(file = "RDS/metadata_covercrop.rds")
-load(file = "RDS/preliminary_results_covercrop.rda")
-
-for (i in 1:length(RESULTS)){
-  
-  RESULTS[[i]]$results.samples <- RESULTS[[i]]$results.samples[1,1:10]
-  RESULTS[[i]]$results.samples <- cbind(RESULTS[[i]]$results.samples, as.data.frame(test[[i]]))
-}
-
-
+# Newly improved results.samples
 RES <- list()
-# fix results.samples
 for (i in 1:length(RESULTS)){
   
-  RES[[i]] <- RESULTS[[i]]$results.samples[1,1:10]
-  RES[[i]] <- cbind(RES[[i]], as.data.frame(test[[i]]))
+  RES[[i]] <- RESULTS[[i]]$results.samples
 }
 
-# View data frame of metrics:
-results.df <- do.call(rbind, RES) %>%
-  dplyr::select("sample.ID", everything())
+# Build combined data frame of metrics
+results.df <- do.call(rbind, RES)
+results.df <- dplyr::relocate(results.df, "sample.ID", .before = "Shannon") # move sample.ID column to leftmost
+# Save data frame of results
+saveRDS(results.df, file = "RDS/results.df_noGreenhouse.rds")
 
 
-# Beta diversity ----
+# test <- readRDS(file = "RDS/metadata_covercrop.rds")
+# load(file = "RDS/preliminary_results_covercrop.rda")
+# 
+# for (i in 1:length(RESULTS)){
+#   
+#   RESULTS[[i]]$results.samples <- RESULTS[[i]]$results.samples[1,1:10]
+#   RESULTS[[i]]$results.samples <- cbind(RESULTS[[i]]$results.samples, as.data.frame(test[[i]]))
+# }
+# 
+# 
+# RES <- list()
+# # fix results.samples
+# for (i in 1:length(RESULTS)){
+#   
+#   RES[[i]] <- RESULTS[[i]]$results.samples[1,1:10]
+#   RES[[i]] <- cbind(RES[[i]], as.data.frame(test[[i]]))
+# }
+# 
+# # View data frame of metrics:
+# results.df <- do.call(rbind, RES) %>%
+#   dplyr::select("sample.ID", everything())
+
